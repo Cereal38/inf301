6b2ab565ef4a6096b65081ffadb0985d216f862b
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2022-09-20 13:09:21 +0200

    client-TUTORIEL

---
+++ README.md
@@ -0,0 +1,9 @@
+
+Lien vers Appolab : http://im2ag-appolab.u-ga.fr
+
+Étapes :
+
+ - client-introduction
+ - client-tutoriel
+ - client-TUTORIEL
+ - client-projetX
--- app1/client-TUTORIEL.c
+++ app1/client-TUTORIEL.c
@@ -6,22 +6,38 @@
 
 #define MAXMSG MAXREP
 
+char* capitalize(char *string) {
+	for (int i = 0; string[i]; i++) {
+		string[i] = toupper(string[i]);
+	}
+	return string;
+}
+
 int main() {
     char reponse[MAXREP]; // pour stocker la réponse du serveur
     char message[MAXMSG]; // pour stocker le message à envoyer au serveur
 
     // Affiche les échanges avec le serveur (false pour désactiver)
-    mode_debug(true);
+    mode_debug(false);
 
     // Connexion au serveur AppoLab
     connexion("im2ag-appolab.u-ga.fr", 9999);
     // utilisez le port 443 en cas de problème sur le 9999
     /* connexion("im2ag-appolab.u-ga.fr", 443); */
 
-    // Remplacez <identifiant> et <mot de passe> ci dessous.
-    envoyer_recevoir("login <identifiant> <mot de passe>", reponse);
-    envoyer_recevoir("load tutoriel", reponse);
-    // Notez qu'ici on n'utilise pas la reponse du serveur
+
+    envoyer_recevoir("login 11800672 \"BRUN-COSME-GAZOT\"", reponse);
+    envoyer_recevoir("load TUTORIEL", reponse);
+    envoyer_recevoir("depart", reponse);
+    envoyer_recevoir("OK", reponse);
+    envoyer_recevoir("OUI", reponse);
+
+	int i;
+	for (i = 0; i < 6; i++) {
+		envoyer_recevoir(capitalize(reponse), reponse);
+		printf("%s", reponse);
+	}
+
 
     lire_clavier(message);   // message tapé au clavier
     envoyer_recevoir(message, reponse); // on envoie message, et on reçoit la réponse du serveur
